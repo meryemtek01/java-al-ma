5ce9770c5592e98eb2410e323ef6bf105cfaee53
meryem <mamfmstekin@gmail.com>
2023-06-18 23:22:01 +0300

    metot calışma

--- src/ch10_MethodCreation/tasks10/Odev03.java
+++ src/ch10_MethodCreation/tasks10/Odev03.java
@@ -16,26 +16,29 @@ public class Odev03 {
         System.out.println("sayı giriniz");
         int sayi= input.nextInt();
 
-        getAsalMi(sayi);
+        boolean asalmi= getAsalMi(sayi);
+        if (asalmi) {
+            System.out.println(sayi + " bir asal sayıdır.");
+        } else {
+            System.out.println(sayi + " bir asal sayı değildir.");
+        }
     }//main sonu
 
     private static boolean getAsalMi(int num) {
-        boolean result;
-        if (num <= 1) {
+
+        if (num < 2) {
             return false;
         }
 
-      //  for (int i = 2; i <= Math.sqrt(num); i++) {
-        for (int i = 2; i < num; i++) {
+      // for (int i = 2; i <= Math.sqrt(num); i++) {
+       for (int i = 2; i < num; i++) {
 
             if (num % i == 0) {
                 return false;
             }
         }
 
+
         return true;
-
-
-
     }
 }//class sonu
